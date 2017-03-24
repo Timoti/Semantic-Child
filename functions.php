966ce6ea402dd99fbb639cb259e0c115c2bd2a33
Tim Gummer <tim@timgummerdesign.com>
2017-03-24 13:55:29 +1300

    functions
    debug to working state
    enque child styles, clear out parent redundancies

--- functions.php
+++ functions.php
@@ -14,49 +14,19 @@
  * @since Semantique Child 1.0.0
  */
 
-/** Various clean up functions */
-require_once( 'library/cleanup.php' );
+function semanticchild_enqueue_styles() {
 
-/** Required for Foundation to work properly */
-require_once( 'library/foundation.php' );
+    $parent_style = 'parent-style'; // This is 'semantic-style' for the Twenty Fifteen theme.
 
-/** Register all navigation menus */
-require_once( 'library/navigation.php' );
-
-/** Add menu walkers for top-bar and off-canvas */
-require_once( 'library/menu-walkers.php' );
-
-/** Create widget areas in sidebar and footer */
-require_once( 'library/widget-areas.php' );
-
-/** Return entry meta information for posts */
-require_once( 'library/entry-meta.php' );
-
-/** Enqueue scripts */
-require_once( 'library/enqueue-scripts.php' );
-
-/** Add theme support */
-require_once( 'library/theme-support.php' );
-
-/** Add Nav Options to Customer */
-require_once( 'library/custom-nav.php' );
-
-/** Change WP's sticky post class */
-require_once( 'library/sticky-posts.php' );
-
-/** Configure responsive image sizes */
-require_once( 'library/responsive-images.php' );
-
-/** If your site requires protocol relative url's for theme assets, uncomment the line below */
-// require_once( 'library/protocol-relative-theme-assets.php' );
+    //wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'semanticchild_enqueue_styles' );
+?>
 
 
-/** Default Image Plugin for dedicated post-type image  */
-// function dfi_posttype_book ( $dfi_id, $post_id ) {
-// 	$post = get_post($post_id);
-// 	if ( 'wiki' === $post->post_type ) {
-// 		return 31; // the image id
-// 	}
-// 	return $dfi_id; // the original featured image id
-// }
-// add_filter( 'dfi_thumbnail_id', 'dfi_posttype_book', 10, 2 );
+
